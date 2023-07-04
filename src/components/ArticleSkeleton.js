ea1082983c0f3f825dab6ff7472e0f5dc07112d2
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-04 10:13:30 +0300

    Updated ArticleSkeleton component

--- src/components/ArticleSkeleton.js
+++ src/components/ArticleSkeleton.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const ArticleSkeleton = () => {
   return (
-    <div>ArticleSkeleton</div>
-  )
-}
+    <div className="skeleton-wrapper">
+      <div className="skeleton-article">
+        <div className="skeleton image" />
 
-export default ArticleSkeleton
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div className="skeleton avatar"></div>
+          <div className="skeleton text"></div>
+        </div>
+
+        <div className="skeleton title"></div>
+
+        <div className="skeleton smalltext"></div>
+      </div>
+      <div className="shimmer-wrapper">
+        <div className="shimmer"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleSkeleton;
